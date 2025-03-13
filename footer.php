ae6a11072e23aa75f9eda8abfacd75b3c2e3edb7
Aditya Sri Ram <aditya22bcs57@iiitkottayam.ac.in>
2025-03-13 10:26:25 +0530

    first commit

--- footer.php
+++ footer.php
@@ -1,56 +1,74 @@
-<footer class="footer text-center"  id="contact">
-            <div class="container">
-                <div class="row">
-                    <!-- Footer Location-->
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Contact us</h4>
-                        <p class="lead mb-0">
-                        Indian Institute Of Information Technology Kottayam, 
-						Valavoor P O, Pala, Kottayam Kerala, India - 686635 <br/>
-						<a style="text-decoration: none; color: white;" href="tel:+91-0482-2202161">
-						<i class="fas fa-phone"></i> &nbsp; Phone: +91-0482-2202171 </a><br/>
-						<a style="text-decoration: none; color: white;" href="mailto:kguravaiah@iiitkottayam.ac.in"><i class="fas fa-envelope"></i> &nbsp; EMAIL</a> <br/>
-					
-                        </p>
-                    </div>
-                    <!-- Footer Social Icons-->
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Around the Web</h4>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/IIITKottayamofficial/"><i class="fab fa-fw fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/IIITKKottayam"><i class="fab fa-fw fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/iiit-kottayam"><i class="fab fa-fw fa-linkedin-in"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.youtube.com/channel/UC3jrKDXjl7_c5bld5x8C1vw"><i class="fab fa-fw fa-youtube"></i></a>
-                    </div>
-                    <!-- Footer About Text-->
-                    <div class="col-lg-4">
-                        <h4 class="text-uppercase mb-4">Links</h4>
-                        <p class="lead mb-0" >
-                            <a style="text-decoration: none; color:white;" href="#home">Home</a><br>
-							<!--<a style="text-decoration: none; color:white;" href="#home">Brochure</a><br>
-							<a style="text-decoration: none; color:white;" href="#home">Personal</a><br>
-							<a style="text-decoration: none; color:white;" href="#home">Conference</a><br>-->
-                        </p>
-                    </div>
+<footer>
+    <div class="footer-wave"></div>
+    <div class="container">
+        <div class="footer-container">
+            <!-- About Section -->
+            <div class="footer-section">
+                <div class="footer-logo">
+                    <img src="assets/img/swing.png" alt="SWING Logo">
+                    <h4>SWING @ IIIT Kottayam</h4>
+                </div>
+                <p>The Smart Wireless Inter-Networking Group (SWING) works on network research by providing smartness using AI, Machine learning and deep learning.</p>
+                <div class="social-icons">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fab fa-github"></i></a>
                 </div>
             </div>
-        </footer>
-<div class="copyright py-2 text-center text-white">
-            <div class="container"> &copy; IIIT Kottayam 2022</div>
+
+            <!-- Quick Links -->
+            <div class="footer-section">
+                <h4>Quick Links</h4>
+                <ul>
+                    <li><a href="home.php"><i class="fas fa-angle-right"></i> Home</a></li>
+                    <li><a href="areas.php"><i class="fas fa-angle-right"></i> Research Areas</a></li>
+                    <li><a href="projects.php"><i class="fas fa-angle-right"></i> Projects</a></li>
+                    <li><a href="events.php"><i class="fas fa-angle-right"></i> Events</a></li>
+                    <li><a href="publications.php"><i class="fas fa-angle-right"></i> Publications</a></li>
+                </ul>
+            </div>
+
+            <!-- Contact Info -->
+            <div class="footer-section">
+                <h4>Contact Us</h4>
+                <div class="footer-contact-info">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <p>IIIT Kottayam, Valavoor P.O, Pala, Kottayam, Kerala, India - 686635</p>
+                </div>
+                <div class="footer-contact-info">
+                    <i class="fas fa-phone"></i>
+                    <p>+91 482 2202 188</p>
+                </div>
+                <div class="footer-contact-info">
+                    <i class="fas fa-envelope"></i>
+                    <p>swing@iiitkottayam.ac.in</p>
+                </div>
+            </div>
+
+            <!-- Newsletter -->
+            <div class="footer-section">
+                <h4>Newsletter</h4>
+                <p>Subscribe to our newsletter to get updates on our latest research and events.</p>
+                <div class="footer-subscription">
+                    <form action="#" method="post">
+                        <input type="email" placeholder="Your Email Address" required>
+                        <button type="submit"><i class="fas fa-paper-plane"></i></button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <!-- Courses Modals-->
-        <!-- Courses Modal 1-->
-       
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-       
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
-		
-		<script src="js/jquery.stellar.min.js"></script>
-		<script src="js/owl.carousel.min.js"></script>
-		<script src="js/aos.js"></script>
-		<script src="js/scrollax.min.js"></script>
-		<script src="js/main.js"></script>
-    </body>
+        
+        <!-- Footer Bottom -->
+        <div class="footer-bottom">
+            <p>&copy; <?php echo date('Y'); ?> SWING Research Group @ IIIT Kottayam. All Rights Reserved. | Designed with <i class="fas fa-heart" style="color: #e25555;"></i> by SWING Team</p>
+        </div>
+    </div>
+    
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</footer>
+</body>
 </html>
--- head.php
+++ head.php
@@ -1,21 +1,20 @@
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
+        <meta name="description" content="Smart Wireless Inter-Networking Research Group at IIIT Kottayam" />
+        <meta name="author" content="SWING Research Group" />
         <title>SWING @ IIIT Kottayam</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		
 		<!-- Core theme CSS (includes Bootstrap)-->
         <link rel="stylesheet" href="css/styles.css"/>
@@ -25,9 +24,12 @@
 		<link rel="stylesheet" href="css/kingcomposer.min.css">
 		<link rel="stylesheet" href="css/extendify-utilities.css">
 		<link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/custom.css">
 	<style>
 	.slider-item{
-		opacity:0.7;
+		opacity:0.85;
+		transition: opacity 0.3s ease;
 	}
 	.slider-item:hover{
 		opacity:1.0;
@@ -37,44 +39,71 @@
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar1 navbar-expand-lg bg-secondary fixed-top" id="mainNav">
-            <div class="container">
-                <div style="vertical-align:middle">
-				<!--<table border="0"><tr>
-				<td><a class="navbar-brand1" href="#page-top" style="vertical-align:middle;">
-					<img src="assets/img/iiitk-logo.jpg" id="rcorners2" /></a><td>-->
-                <button class="navbar-toggler font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-				</div>
-                <div class="collapse1 navbar-collapse1" id="navbarResponsive">
-                    <ul class="navbar-nav1 ms-auto">
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="navbar-brand1" href="#page-top" style="vertical-align:middle;">
-					<img src="assets/img/iiitk-logo.jpg" id="rcorners2" /></a></li>
-						
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="nav-link1 py-3 px-0 px-lg-3 rounded" href="home.php#people">Home</a></li>
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="nav-link1 py-3 px-0 px-lg-3 rounded" href="areas.php#areas">Areas</a></li>
-                        
-						
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="nav-link1 py-3 px-0 px-lg-3 rounded" href="projects.php">Funded Projects</a></li>
-						
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="nav-link1 py-3 px-0 px-lg-3 rounded" href="events.php">Events</a></li>
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="nav-link1 py-3 px-0 px-lg-3 rounded" href="publications.php">Publications</a></li>
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="nav-link1 py-3 px-0 px-lg-3 rounded" href="talks.php">Talks</a></li>
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="nav-link1 py-3 px-0 px-lg-3 rounded" href="gallery.php">Gallery</a></li>
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="nav-link1 py-3 px-0 px-lg-3 rounded" href="downloads.php">Downloads</a></li>
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="nav-link1 py-3 px-0 px-lg-3 rounded" href="contacts.php">Contact</a></li>
-						<li class="nav-item1 mx-0 mx-lg-1"><a class="navbar-brand1" href="#page-top" style="vertical-align:middle;"><img src="assets/img/swing.png" id="rcorners2" /></a></li>
-                    </ul>
+        <nav class="navbar1 navbar-expand-lg fixed-top" id="mainNav">
+            <div class="container navbar-container">
+                <!-- Left Logo -->
+                <div class="logo-container-left">
+                    <a class="navbar-brand1" href="#page-top">
+                        <img src="assets/img/iiitk-logo.jpg" id="rcorners2" alt="IIIT Kottayam Logo"/>
+                    </a>
                 </div>
-				<!--</tr>
-				</table>-->
+                
+                <!-- Mobile Toggle Button -->
+                <button class="navbar-toggle-btn d-lg-none" type="button" id="navbarToggleBtn">
+                    <i class="fas fa-bars me-1"></i> Menu
+                </button>
+                
+                <!-- Center Navigation -->
+                <div class="nav-center-container">
+                    <div class="navbar-collapse1" id="navbarResponsive">
+                        <ul class="navbar-nav1">
+                            <li class="nav-item1"><a class="nav-link1 rounded" href="home.php#people">Home</a></li>
+                            <li class="nav-item1"><a class="nav-link1 rounded" href="areas.php#areas">Areas</a></li>
+                            <li class="nav-item1"><a class="nav-link1 rounded" href="projects.php">Funded Projects</a></li>
+                            <li class="nav-item1"><a class="nav-link1 rounded" href="events.php">Events</a></li>
+                            <li class="nav-item1"><a class="nav-link1 rounded" href="publications.php">Publications</a></li>
+                            <li class="nav-item1"><a class="nav-link1 rounded" href="talks.php">Talks</a></li>
+                            <li class="nav-item1"><a class="nav-link1 rounded" href="gallery.php">Gallery</a></li>
+                            <li class="nav-item1"><a class="nav-link1 rounded" href="downloads.php">Downloads</a></li>
+                            <li class="nav-item1"><a class="nav-link1 rounded" href="contacts.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <!-- Right Logo -->
+                <div class="logo-container-right">
+                    <a class="navbar-brand1" href="#page-top">
+                        <img src="assets/img/swing.png" id="rcorners2" alt="SWING Logo"/>
+                    </a>
+                </div>
             </div>
         </nav>
 		
+        <!-- Mobile Menu Toggle Script -->
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const navbarToggleBtn = document.getElementById('navbarToggleBtn');
+                const navbarResponsive = document.getElementById('navbarResponsive');
+                
+                navbarToggleBtn.addEventListener('click', function() {
+                    navbarResponsive.classList.toggle('show');
+                });
+                
+                // Add active class to current nav item
+                const currentLocation = window.location.pathname;
+                const navLinks = document.querySelectorAll('.nav-link1');
+                navLinks.forEach(link => {
+                    const linkPath = link.getAttribute('href').split('#')[0];
+                    if (currentLocation.includes(linkPath) && linkPath !== '') {
+                        link.classList.add('active');
+                    } else if (currentLocation.endsWith('/') && link.getAttribute('href').includes('home.php')) {
+                        link.classList.add('active');
+                    }
+                });
+            });
+        </script>
+        
         <!-- Masthead-->
-		
       <header class="masthead2 text-white text-center" id="home">
       <section class="home-slider owl-carousel" style="margin-bottom: -35px;">
 		  <div class="slider-item" style="background-image:url(assets/img/clg.jpeg);">
